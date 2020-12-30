a44a4bcd05fd8377305fb400dbac5ae2aa492c8b
Luke Thompson <lukethompson@gmail.com>
2020-12-30 17:36:30 -0500

    changed chao1 to pielou_e and minor edits to README

--- scripts/initialize_dir_from_existing_tourmaline_dir.sh
+++ scripts/initialize_dir_from_existing_tourmaline_dir.sh
@@ -3,13 +3,11 @@
 # Usage: 
 # scripts/initialize_dir_from_existing_tourmaline_dir.sh /path/to/old/tourmaline
 #
-# From the main directory of a newly cloned tourmaline directory, run this
-# script to copy to config.yaml and Snakefile from an existing tourmaline
-# directory, remove the test files, then copy the data files and symlinks
-# from the existing tourmaline directory.
+# From the main directory of a newly cloned tourmaline directory, run this script
+# to copy config.yaml from an existing tourmaline directory, remove the test files,
+# then copy the data files and symlinks from the existing tourmaline directory.
 
 cp -a $1/config.yaml .
-rsync -a $1/Snakefile .
 /bin/rm -r 00-data/*
 cp -a $1/00-data/manifest_pe.csv 00-data/manifest_pe.csv
 cp -a $1/00-data/manifest_se.csv 00-data/manifest_se.csv
